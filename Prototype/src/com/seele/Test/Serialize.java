753cd72f7440ac8c1cfbb5591ac1c270f461ec7b
homurachyan <homurachyan@gmail.com>
2015-05-29 22:12:09 +0800

    unSerializable

--- Prototype/src/com/seele/Test/Serialize.java
+++ Prototype/src/com/seele/Test/Serialize.java
@@ -1,7 +1,9 @@
 package com.seele.Test;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
@@ -14,7 +16,7 @@ public class Serialize implements Serializable {
 	   System.out.println("lalala");
    }
    
-   public static void main(String[] args) {
+   public static void main(String[] args) throws Exception {
 	   try{
 		   FileOutputStream fos = new FileOutputStream("s.txt");
 		   ObjectOutputStream oos = new ObjectOutputStream(fos);
@@ -23,6 +25,14 @@ public class Serialize implements Serializable {
 		   oos.flush();
 		   oos.close();
 		   
+	   } catch(IOException e){
+		   e.printStackTrace();  
+	   }
+	   try{
+		   FileInputStream fos = new FileInputStream("s.txt");
+		   ObjectInputStream ois = new ObjectInputStream(fos);
+		   Serialize s1 = (Serialize) ois.readObject();
+		   System.out.println(s1.num);  
 		   
 	   } catch(IOException e){
 		   e.printStackTrace();  
