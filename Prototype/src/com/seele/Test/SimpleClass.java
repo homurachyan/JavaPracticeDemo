affdbc05169aa3f7a459598058968b8e3b8bd5ed
homurachyan <homurachyan@gmail.com>
2015-06-08 23:42:12 +0800

    change some reflect demo

--- Prototype/src/com/seele/Test/SimpleClass.java
+++ Prototype/src/com/seele/Test/SimpleClass.java
@@ -1,9 +1,19 @@
 package com.seele.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class SimpleClass {
-	 public void sayHello() {
-			System.out.println("Hello");
+	 public void sayHello(String words) {
+			System.out.println(words);
 			System.getProperty("java.class.path");
 	}
-
+	 
+    public static void main(String[] args) throws ClassNotFoundException, IllegalArgumentException, IllegalAccessException, InvocationTargetException{
+    	SimpleClass sc = new SimpleClass();
+    	Class clz = sc.getClass();
+    	Method[] ms = clz.getDeclaredMethods();
+    	ms[0].invoke(sc,"Acfun");
+  	
+    }
 }
